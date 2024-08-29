14a93c000c377d0743e04909e0e79acb64ea4d1b
“sunnytooltsmiths <xx252@cornell.edu>
2024-08-28 17:17:39 -0700

    load the file to csv

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -15,19 +17,34 @@ print(f"Found {len(investor_links)} investor links.")
 for link in investor_links:
     print(link)
 
+investor_file = open('investor_file.csv','a')
+investor_file.write('investor_name,investor_company,ticker,stock,percent_portfolio,shares,reported_price,percent_difference_current_price,current_price,value,percent_activity,percent_change_to_portfolio\n')
+
+
 # step 2: Iterate over each investor's page
 for investor_url in investor_links:
+    time.sleep(1)
     i = 1
     guru_done = False
     while not guru_done:
 
         url = f'{investor_url}?sells_page=1&page={i}'
-        response = requests.get(url)
+        bad_response = True
+        while bad_response:
+            response = requests.get(url)
+            print(response.status_code)
+            if response.status_code == 200:
+                    bad_response = False
+            else:
+                print('bad response: ', response.status_code)
+                time.sleep(60)
+
         content = response.text
 
         soup = BeautifulSoup(content,'lxml')
 
         breadcrumb = soup.find('div', class_='breadcrumb-content d-flex flex-column align-items-center text-center')
+
 
         name = breadcrumb.find('h2', class_='tag-h2 text-white text-uppercase').text.strip()
         # Extract Berkshire Hathaway
@@ -45,10 +62,20 @@ for investor_url in investor_links:
             shares_and_reported_price = row.find_all('div', class_ ='guru_table_column')
             shares = shares_and_reported_price[4].text.strip()
             reported_price = shares_and_reported_price[5].text.strip()
-            percent_difference_current_price = row.find ('span', class_ = 'pl-1 pr-2').text.strip()
-            current_price = shares_and_reported_price[6].find_all('span')[-1].text
+
+            percent_difference_current_price = row.find('span', class_='pl-1 pr-2')
+            if percent_difference_current_price is None:
+                percent_difference_current_price = '0'
+            else:
+                percent_difference_current_price = percent_difference_current_price.text.strip()
+
+            current_price = shares_and_reported_price[6].find_all('span')
+            if not current_price:
+                current_price = '0'
+            else:
+                current_price = current_price[-1].text
             value = shares_and_reported_price[7].text.strip()
-            percent_activity = row.find('span', class_ = 'normal')
+            percent_activity = row.find('span', class_='normal')
             if percent_activity:
                 percent_activity = percent_activity.text.strip()
             else:
@@ -58,7 +85,12 @@ for investor_url in investor_links:
                 percent_change_to_portfolio = percent_change_to_portfolio.text.strip()
             else:
                 percent_change_to_portfolio = '0'  # Default to '0' if percent_activity is empty
-            print(ticker, stock, percent_portfolio, shares, reported_price, percent_difference_current_price,current_price, value, percent_activity, percent_change_to_portfolio)
+            print(ticker, stock, percent_portfolio, shares, reported_price, percent_difference_current_price,
+                  current_price, value, percent_activity, percent_change_to_portfolio)
+            investor_file.write(
+                f'{name},{company},{ticker},{stock},{percent_portfolio},{shares},{reported_price},{percent_difference_current_price},{current_price},{value},{percent_activity},{percent_change_to_portfolio}\n')
         print(f'Pge {i} done')
 
         i += 1
+
+investor_file.close()
